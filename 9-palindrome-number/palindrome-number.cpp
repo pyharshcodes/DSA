aaa66e6b5e2a3194b76f5c7ccdd89174d3c74ef2
Harsh Deep Chak <harshdeepchak@gmail.com>
2026-07-25 21:38:41 +0530

    Time: 3 ms (44.47%) | Memory: 11 MB (7.47%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        std::string s = std::to_string(x);
+        int left=0;
+        int right=s.size()-1;
+        while(left<right){
+            if(s[left]!=s[right]){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+};
